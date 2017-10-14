f840cb0e283151a918539593593c685015cabb78
Takuma Homma <nagomimatcha@gmail.com>
2017-10-14 19:30:40 +0900

    Update val

--- lib/ruboty/tweet/actions/post.rb
+++ lib/ruboty/tweet/actions/post.rb
@@ -18,7 +18,7 @@ module Ruboty
             c.consumer_key = ENV['TWITTER_CONSUMER_KEY']
             c.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
             c.access_token = ENV['TWITTER_ACCESS_TOKEN']
-            c.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+            c.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
           end
         end
 
